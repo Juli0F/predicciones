c8d364cba39b3229209c72d83f4b01750aca6fef
Juli0F <53358780+Juli0F@users.noreply.github.com>
2021-12-25 22:34:33 -0600

    instruccion que genera la grafica

--- venta.py
+++ venta.py
@@ -14,13 +14,14 @@ modelo = LinearRegression();
 modelo.fit(X=dataframe[independientes],y=dataframe[target])
 
 dataframe["predicted"] = modelo.predict(dataframe[independientes])
-prediccion_test = dataframe[["monto","prediccion"]].head(50)#50 es un numero arbitrario
+prediccion_test = dataframe[["monto","predicted"]].head(50)#50 es un numero arbitrario
 
 predicted = modelo.predict([[41,1,1,1]])
 print("Posibilidades de Comprar :",predicted);
 
 
-predicted.plot(kind='bar',figsize=(20,10))
-#plt.grid
+dataframe[["monto", "predicted"]].head(50).plot(kind='bar',figsize=(19,7))
+plt.grid(linewidth='2')
+plt.grid(linewidth='2')
 plt.grid(None)
 plt.show()
